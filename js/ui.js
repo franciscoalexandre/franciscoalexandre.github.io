c72c6e82e8f09d43c4a71dd78182bb23670b83df
franciscoalexandre <55537630+franciscoalexandre@users.noreply.github.com>
2019-11-21 07:58:20 -0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,4 @@
+const sobremesas = document.querySelector('.sobremesas');
 document.addEventListener('DOMContentLoaded', function() {
 // menus laterais 
     var elems = document.querySelectorAll('.sidenav');
@@ -7,3 +8,26 @@ document.addEventListener('DOMContentLoaded', function() {
 	const forms = document.querySelectorAll('.side-form');
 	var instances2 = M.Sidenav.init(forms, {edge:'right'});
 });
+const desenhaCard = (data, id) => {
+
+    const html = `
+  <div class="col s12 m6 l3 servico" data-id="${id}">
+	    <div class="card" style="background-color: #A59C94FF;">
+		    <div class="card-image servico-imagem">
+	            <img src="images/${data.endereco_imagem}">
+	            <span class="card-title servico-titulo">"${data.nome}"</span>
+	        </div>
+	        <div class="card-content servico-descricao">
+	            <p>"${data.descricao}"</p>
+	        </div>
+	        <div class="card-action">
+		        <a style="color: #AE0E36FF;" class="servico-link" href="${data.link}">Saiba mais</a>
+	        </div>
+	        <div class="servico-deletar">
+                <i class="material-icons" data-id="${id}">delete_outline</i>
+            </div>
+	    </div>
+   </div>`;
+    sobremesas.innerHTML += html;
+
+};
