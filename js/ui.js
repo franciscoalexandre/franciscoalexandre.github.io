f9d2450aa9d8e877c46ef9b8943f65e09dd4073a
franciscoalexandre <55537630+franciscoalexandre@users.noreply.github.com>
2019-11-21 07:56:16 -0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-	var elems = document.querySelectorAll('.sidenav');
-	var instances = M.Sidenav.init(elems);
+// menus laterais 
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {edge:'left'});
+
+	// formulario para adicionar sobremesa
+	const forms = document.querySelectorAll('.side-form');
+	var instances2 = M.Sidenav.init(forms, {edge:'right'});
 });
