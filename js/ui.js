e16bcb84082d3602eaf69622cf33ca2f0da02ea0
franciscoalexandre <55537630+franciscoalexandre@users.noreply.github.com>
2019-12-05 08:00:55 -0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,4 @@
-const sobremesas = document.querySelector('.sobremesas');
+const barbearia = document.querySelector('.barbearia');
 document.addEventListener('DOMContentLoaded', function() {
 // menus laterais 
     var elems = document.querySelectorAll('.sidenav');
@@ -8,26 +8,33 @@ document.addEventListener('DOMContentLoaded', function() {
 	const forms = document.querySelectorAll('.side-form');
 	var instances2 = M.Sidenav.init(forms, {edge:'right'});
 });
+
 const desenhaCard = (data, id) => {
 
     const html = `
   <div class="col s12 m6 l3 servico" data-id="${id}">
 	    <div class="card" style="background-color: #A59C94FF;">
 		    <div class="card-image servico-imagem">
-	            <img src="images/${data.endereco_imagem}">
-	            <span class="card-title servico-titulo">"${data.nome}"</span>
+	            <img src="images/${data.imagem}">
+	            <span class="card-title servico-titulo">"${data.Titulo}"</span>
 	        </div>
 	        <div class="card-content servico-descricao">
-	            <p>"${data.descricao}"</p>
+	            <p>"${data.Descricao}"</p>
 	        </div>
 	        <div class="card-action">
-		        <a style="color: #AE0E36FF;" class="servico-link" href="${data.link}">Saiba mais</a>
+		        <a style="color: #AE0E36FF;" class="servico-link" href="${data.Link}">Saiba mais</a>
 	        </div>
 	        <div class="servico-deletar">
                 <i class="material-icons" data-id="${id}">delete_outline</i>
             </div>
 	    </div>
    </div>`;
-    sobremesas.innerHTML += html;
+    barbearia.innerHTML += html;
 
+};
+
+// remove recipe
+const removeCard = (id) => {
+  const servico = document.querySelector(`.servico[data-id=${id}]`);
+  servico.remove();
 };
